48448707c992576bd36170577d6bc32590676683
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-01 09:36:08 -0500

    fix(lib): make generators module public and re-export error types

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod braid;
-mod generators;
+pub mod generators;
 mod index;
 mod macros;
 mod sign;
 mod strand;
 
 pub use braid::{Braid, BraidValidationError};
-pub use generators::{ArtinGenerator, BandGenerator};
+pub use generators::{ArtinGenerator, ArtinValidationError, BandGenerator, BandValidationError};
 pub use index::BraidIndex;
 pub use sign::Sign;
 pub use strand::Strand;
