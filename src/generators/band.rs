82ab05e6ba207342a59684bff2d54b2cdbdfe576
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-04 13:43:16 -0500

    test(generators::band): implement unit test suite

--- src/generators/band.rs
+++ src/generators/band.rs
@@ -19,7 +19,7 @@ pub enum FromArtinError {
     #[error("Even number of Artin generators provided.")]
     EvenGenerators,
     #[error("Could not append {next_step:?} to {previous_step:?} in {quadrant:?} staircase.")]
-    IncompatibleSteps {
+    IncontiguousSteps {
         quadrant: StaircaseQuadrant,
         next_step: ArtinGenerator,
         previous_step: ArtinGenerator,
@@ -94,7 +94,7 @@ impl BandGenerator {
                         upper_left_staircase.push(*left_part);
                     } else {
                         return Err(BandValidationError::from(
-                            FromArtinError::IncompatibleSteps {
+                            FromArtinError::IncontiguousSteps {
                                 quadrant: StaircaseQuadrant::UpperLeft,
                                 next_step: *left_part,
                                 previous_step: *previous_step,
@@ -108,7 +108,7 @@ impl BandGenerator {
                         lower_left_staircase.push(*left_part);
                     } else {
                         return Err(BandValidationError::from(
-                            FromArtinError::IncompatibleSteps {
+                            FromArtinError::IncontiguousSteps {
                                 quadrant: StaircaseQuadrant::LowerLeft,
                                 next_step: *left_part,
                                 previous_step: *previous_step,
@@ -124,7 +124,7 @@ impl BandGenerator {
                         lower_right_staircase.push(*right_part);
                     } else {
                         return Err(BandValidationError::from(
-                            FromArtinError::IncompatibleSteps {
+                            FromArtinError::IncontiguousSteps {
                                 quadrant: StaircaseQuadrant::LowerRight,
                                 next_step: *right_part,
                                 previous_step: *previous_step,
@@ -138,7 +138,7 @@ impl BandGenerator {
                         upper_right_staircase.push(*right_part);
                     } else {
                         return Err(BandValidationError::from(
-                            FromArtinError::IncompatibleSteps {
+                            FromArtinError::IncontiguousSteps {
                                 quadrant: StaircaseQuadrant::UpperRight,
                                 next_step: *right_part,
                                 previous_step: *previous_step,
@@ -184,6 +184,18 @@ impl BandGenerator {
             sign: -self.sign,
         }
     }
+    pub fn height(&self) -> u16 {
+        (self.head - self.foot).unwrap().into()
+    }
+    pub fn is_artin(&self) -> bool {
+        self.height() == 1
+    }
+    pub fn minimal_required_braid_index(&self) -> BraidIndex {
+        BraidIndex::new(self.head).unwrap()
+    }
+    pub fn artin_length(&self) -> u16 {
+        2 * self.height() - 1
+    }
 
     pub fn decompose(&self) -> Vec<ArtinGenerator> {
         // Band decomposition is infallible, so it's safe to unwrap any intermediate results
@@ -196,19 +208,6 @@ impl BandGenerator {
         }
         let right = left.iter().rev().map(|a| a.inverse()).collect();
         [left, vec![crossing], right].concat()
-    }
-
-    pub fn height(&self) -> u16 {
-        (self.head - self.foot).unwrap().into()
-    }
-    pub fn is_artin(&self) -> bool {
-        self.height() == 1
-    }
-    pub fn minimal_required_braid_index(&self) -> BraidIndex {
-        BraidIndex::new(self.head).unwrap()
-    }
-    pub fn artin_length(&self) -> u16 {
-        1 + (self.height() - 1) * 2
     }
 }
 
@@ -226,6 +225,287 @@ impl From<Letter> for BandGenerator {
         match value {
             Letter::Artin(artin) => Self::from(artin),
             Letter::Band(band) => band,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{FromArtinError, StaircaseQuadrant};
+    use crate::{
+        ArtinGenerator, BandGenerator, BandValidationError, BraidIndex, Letter, Sign, Strand,
+    };
+    use googletest::matchers::{anything, each, eq, err, ok};
+    use googletest::{assert_that, expect_that, gtest};
+
+    #[test]
+    fn valid_inputs_to_new_yield_successful_construction() {
+        let valid_bands = [
+            BandGenerator::new(3, 4, Sign::Positive),
+            BandGenerator::new(1, u16::MAX, Sign::Negative),
+            BandGenerator::new(2_usize, 5_isize, Sign::Positive),
+            BandGenerator::new(Strand::new(9).unwrap(), 40_u32, Sign::Negative),
+            BandGenerator::new(-(-3), Strand::new(10).unwrap(), Sign::Positive),
+        ];
+
+        assert_that!(valid_bands, each(ok(anything())));
+    }
+
+    #[test]
+    fn valid_inputs_to_from_yield_expected_construction() {
+        let expected_band = BandGenerator::new(1, 2, Sign::Positive).unwrap();
+        let test_bands = [
+            BandGenerator::from(ArtinGenerator::new(1, Sign::Positive).unwrap()),
+            BandGenerator::from(Letter::new(1, None::<u16>, Sign::Positive).unwrap()),
+            BandGenerator::from(Letter::new(1, Some(2), Sign::Positive).unwrap()),
+        ];
+
+        assert_that!(test_bands, each(eq(expected_band)));
+    }
+
+    #[gtest]
+    fn valid_inputs_to_coalesce_yield_successful_construction() {
+        expect_that!(
+            BandGenerator::coalesce(&[ArtinGenerator::new(1, Sign::Negative).unwrap()]),
+            eq(&BandGenerator::new(1, 2, Sign::Negative))
+        );
+
+        let test_band = BandGenerator::new(1, 4, Sign::Positive);
+        let valid_bands = [
+            BandGenerator::coalesce(&[
+                ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                ArtinGenerator::new(1, Sign::Positive).unwrap(),
+            ]),
+            BandGenerator::coalesce(&[
+                ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                ArtinGenerator::new(3, Sign::Negative).unwrap(),
+            ]),
+            BandGenerator::coalesce(&[
+                ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ArtinGenerator::new(3, Sign::Negative).unwrap(),
+            ]),
+            BandGenerator::coalesce(&[
+                ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ArtinGenerator::new(3, Sign::Negative).unwrap(),
+            ]),
+        ];
+
+        expect_that!(valid_bands, each(eq(&test_band)));
+    }
+
+    #[gtest]
+    fn properties_compute_as_expected() {
+        let input_data = [(1, 2, Sign::Positive), (2, 5, Sign::Negative)];
+
+        for (foot, head, sign) in input_data {
+            let band = BandGenerator::new(foot, head, sign).unwrap();
+
+            expect_that!(band.foot(), eq(Strand::new(foot).unwrap()));
+            expect_that!(band.head(), eq(Strand::new(head).unwrap()));
+            expect_that!(band.sign(), eq(sign));
+            expect_that!(
+                band.inverse(),
+                eq(BandGenerator::new(foot, head, -sign).unwrap())
+            );
+            expect_that!(band.height(), eq(head - foot));
+            expect_that!(band.is_artin(), eq(head - foot == 1));
+            expect_that!(
+                band.minimal_required_braid_index(),
+                eq(BraidIndex::new(head).unwrap())
+            );
+            expect_that!(band.artin_length(), eq(2 * (head - foot) - 1));
+        }
+    }
+
+    #[gtest]
+    fn decomposition_works_as_expected() {
+        let expected_results = [
+            (
+                BandGenerator::new(1, 2, Sign::Positive),
+                vec![ArtinGenerator::new(1, Sign::Positive).unwrap()],
+            ),
+            (
+                BandGenerator::new(1, 4, Sign::Positive),
+                vec![
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ],
+            ),
+            (
+                BandGenerator::coalesce(&[
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ]),
+                vec![
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ],
+            ),
+            (
+                BandGenerator::coalesce(&[
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(3, Sign::Negative).unwrap(),
+                ]),
+                vec![
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ],
+            ),
+        ];
+
+        for (band, expected_decomposition) in expected_results {
+            expect_that!(band.unwrap().decompose(), eq(&expected_decomposition));
+        }
+    }
+
+    #[gtest]
+    fn invalid_inputs_to_new_yield_failure() {
+        let invalid_bands = [
+            (
+                BandGenerator::new(1, 1, Sign::Positive),
+                BandValidationError::FootOnHead(Strand::new(1).unwrap()),
+            ),
+            (
+                BandGenerator::new(4, 1, Sign::Negative),
+                BandValidationError::FootOverHead {
+                    foot: Strand::new(4).unwrap(),
+                    head: Strand::new(1).unwrap(),
+                },
+            ),
+            (
+                BandGenerator::new(0, 4, Sign::Negative),
+                BandValidationError::from(Strand::new(0).err().unwrap()),
+            ),
+            (
+                BandGenerator::new(-1, 4, Sign::Positive),
+                BandValidationError::from(Strand::new(-1).err().unwrap()),
+            ),
+            (
+                BandGenerator::new(1, u16::MAX as u32 + 1, Sign::Negative),
+                BandValidationError::from(Strand::new(u16::MAX as u32 + 1).err().unwrap()),
+            ),
+        ];
+
+        for (invalid_band, error) in invalid_bands {
+            expect_that!(invalid_band, err(eq(&error)));
+        }
+    }
+
+    #[gtest]
+    fn invalid_inputs_to_coalesce_yield_failure() {
+        let invalid_artin_lists: [(Vec<ArtinGenerator>, BandValidationError); 7] = [
+            (vec![], FromArtinError::NoGenerators.into()),
+            (
+                vec![
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                ],
+                FromArtinError::EvenGenerators.into(),
+            ),
+            (
+                vec![
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ],
+                FromArtinError::IncontiguousSteps {
+                    quadrant: StaircaseQuadrant::LowerLeft,
+                    next_step: ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    previous_step: ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                }
+                .into(),
+            ),
+            (
+                vec![
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Negative).unwrap(),
+                ],
+                FromArtinError::IncontiguousSteps {
+                    quadrant: StaircaseQuadrant::UpperLeft,
+                    next_step: ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    previous_step: ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                }
+                .into(),
+            ),
+            (
+                vec![
+                    ArtinGenerator::new(1, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                ],
+                FromArtinError::IncontiguousSteps {
+                    quadrant: StaircaseQuadrant::LowerRight,
+                    next_step: ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                    previous_step: ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                }
+                .into(),
+            ),
+            (
+                vec![
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(3, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                ],
+                FromArtinError::IncontiguousSteps {
+                    quadrant: StaircaseQuadrant::UpperRight,
+                    next_step: ArtinGenerator::new(3, Sign::Negative).unwrap(),
+                    previous_step: ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                }
+                .into(),
+            ),
+            (
+                vec![
+                    ArtinGenerator::new(4, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Negative).unwrap(),
+                    ArtinGenerator::new(3, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(2, Sign::Positive).unwrap(),
+                    ArtinGenerator::new(1, Sign::Positive).unwrap(),
+                ],
+                FromArtinError::ImbalancedStaircases(1).into(),
+            ),
+        ];
+
+        for (invalid_artin_list, error) in invalid_artin_lists {
+            expect_that!(
+                BandGenerator::coalesce(&invalid_artin_list),
+                err(eq(&error))
+            );
         }
     }
 }
