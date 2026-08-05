54ffdbe235f99dc01eff478b4453b924b4b89c75
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-05 09:54:06 -0500

    test(word): implement unit test suite

--- src/word.rs
+++ src/word.rs
@@ -412,5 +412,256 @@ mod tests {
         fn as_ref_tester<W: AsRef<[Letter]>>(w: W, v: &[Letter]) -> bool {
             w.as_ref() == v
         }
+        let letters = [
+            Letter::new(1, Some(3), Sign::Positive).unwrap(),
+            Letter::new(2, None::<u16>, Sign::Negative).unwrap(),
+            Letter::new(1, Some(2), Sign::Positive).unwrap(),
+        ];
+        let word = Word::try_from(&letters[..]).unwrap();
+
+        assert_that!(
+            word,
+            result_of_ref!(|w: &Word| as_ref_tester(w, &letters), is_true()),
+        );
+    }
+
+    #[gtest]
+    fn valid_multiplication_with_letter_succeeds_and_computes_as_expected() {
+        let letters = vec![
+            Letter::new(1, Some(3), Sign::Positive).unwrap(),
+            Letter::new(2, None::<u16>, Sign::Negative).unwrap(),
+            Letter::new(1, Some(2), Sign::Positive).unwrap(),
+        ];
+        let word = Word::try_from(&letters[..]).unwrap();
+        let other_letter = Letter::new(3, Some(7), Sign::Negative).unwrap();
+
+        expect_that!(
+            word.clone() * other_letter,
+            eq(&Word::try_from(
+                [letters.clone(), vec![other_letter]].concat()
+            ))
+        );
+        expect_that!(
+            other_letter * word,
+            eq(&Word::try_from([vec![other_letter], letters].concat()))
+        );
+    }
+
+    #[gtest]
+    fn valid_multiplication_with_word_succeeds_and_computes_as_expected() {
+        let letters1 = vec![
+            Letter::new(1, Some(3), Sign::Positive).unwrap(),
+            Letter::new(2, None::<u16>, Sign::Negative).unwrap(),
+            Letter::new(1, Some(2), Sign::Positive).unwrap(),
+        ];
+        let letters2 = vec![
+            Letter::new(3, None::<u16>, Sign::Negative).unwrap(),
+            Letter::new(2, Some(7), Sign::Negative).unwrap(),
+        ];
+
+        let word1 = Word::try_from(&letters1[..]).unwrap();
+        let word2 = Word::try_from(&letters2[..]).unwrap();
+
+        expect_that!(
+            word1.clone() * word2.clone(),
+            eq(&Word::try_from(
+                [letters1.clone(), letters2.clone()].concat()
+            ))
+        );
+        expect_that!(
+            word2 * word1,
+            eq(&Word::try_from([letters2, letters1].concat()))
+        );
+    }
+
+    #[gtest]
+    fn trivial_word_is_multiplicative_identity() {
+        let letter = Letter::new(1, None::<u16>, Sign::Positive).unwrap();
+
+        expect_that!(
+            letter * Word::trivial(),
+            ok(eq(&Word::try_from(vec![letter]).unwrap()))
+        );
+        expect_that!(
+            Word::trivial() * letter,
+            ok(eq(&Word::try_from(vec![letter]).unwrap()))
+        );
+
+        let word = Word::try_from(vec![
+            Letter::new(1, Some(3), Sign::Positive).unwrap(),
+            Letter::new(2, None::<u16>, Sign::Negative).unwrap(),
+            Letter::new(1, Some(2), Sign::Positive).unwrap(),
+        ])
+        .unwrap();
+
+        expect_that!(word.clone() * Word::trivial(), ok(eq(&word)));
+        expect_that!(Word::trivial() * word.clone(), ok(eq(&word)))
+    }
+
+    #[gtest]
+    fn multiplication_with_inverse_yields_trivial() {
+        let word = Word::try_from(vec![
+            Letter::new(1, Some(3), Sign::Positive).unwrap(),
+            Letter::new(2, None::<u16>, Sign::Negative).unwrap(),
+            Letter::new(1, Some(2), Sign::Positive).unwrap(),
+        ])
+        .unwrap();
+
+        expect_that!(word.clone() * word.inverse(), ok(eq(&Word::trivial())));
+        expect_that!(word.inverse() * word.clone(), ok(eq(&Word::trivial())));
+    }
+
+    #[gtest]
+    fn invalid_construction_with_new_fails() {
+        let invalid_words = [
+            (
+                Word::new(vec![
+                    (1, None::<u16>, Sign::Negative);
+                    u16::MAX as usize + 1
+                ]),
+                WordValidationError::TooLong(u16::MAX as u32 + 1),
+            ),
+            (
+                Word::new([
+                    (1, Some(3), Sign::Positive),
+                    (2, None, Sign::Negative),
+                    (4, Some(1), Sign::Positive),
+                ]),
+                WordValidationError::from(Letter::new(4, Some(1), Sign::Positive).err().unwrap()),
+            ),
+            (
+                Word::new([
+                    (-1, Some(3), Sign::Positive),
+                    (2, None, Sign::Negative),
+                    (1, Some(2), Sign::Positive),
+                ]),
+                WordValidationError::from(Letter::new(-1, Some(3), Sign::Positive).err().unwrap()),
+            ),
+        ];
+
+        for (invalid_word, error) in invalid_words {
+            expect_that!(invalid_word, err(eq(&error)));
+        }
+    }
+
+    #[gtest]
+    fn invalid_construction_with_try_from_fails() {
+        let invalid_words = [
+            Word::try_from(vec![
+                Letter::new(1, None::<u16>, Sign::Negative).unwrap();
+                u16::MAX as usize + 1
+            ]),
+            Word::try_from(
+                &[
+                    Letter::new(1, None::<u16>, Sign::Negative).unwrap(),
+                    Letter::new(1, Some(2u16.pow(15) + 1), Sign::Positive).unwrap(),
+                ][..],
+            ),
+        ];
+
+        for invalid_word in invalid_words {
+            expect_that!(
+                invalid_word,
+                err(eq(&WordValidationError::TooLong(u16::MAX as u32 + 1)))
+            );
+        }
+    }
+
+    #[gtest]
+    fn invalid_mult_with_letter_fails() {
+        let short_word = Word::new([
+            (1, Some(3), Sign::Positive),
+            (2, None, Sign::Negative),
+            (1, Some(2), Sign::Positive),
+        ])
+        .unwrap();
+        let long_word = Word::try_from(vec![
+            Letter::new(1, None::<u16>, Sign::Positive).unwrap();
+            u16::MAX as usize
+        ])
+        .unwrap();
+        let short_letter = Letter::new(2, None::<u16>, Sign::Negative).unwrap();
+        let tall_letter = Letter::new(1, Some(2usize.pow(15) + 1), Sign::Positive).unwrap();
+
+        let invalid_products = [
+            (
+                short_word.clone() * tall_letter,
+                u16::MAX as u32 + 5,
+                "short_word * tall_letter",
+            ),
+            (
+                tall_letter * short_word,
+                u16::MAX as u32 + 5,
+                "tall_letter * short_word",
+            ),
+            (
+                long_word.clone() * short_letter,
+                u16::MAX as u32 + 1,
+                "long_word * short_letter",
+            ),
+            (
+                short_letter * long_word.clone(),
+                u16::MAX as u32 + 1,
+                "short_letter * long_word",
+            ),
+            (
+                long_word.clone() * tall_letter,
+                2 * (u16::MAX as u32),
+                "long_word * tall_letter",
+            ),
+            (
+                tall_letter * long_word,
+                2 * (u16::MAX as u32),
+                "tall_letter * long_word",
+            ),
+        ];
+
+        for (invalid_product, length, label) in invalid_products {
+            expect_that!(
+                invalid_product,
+                err(eq(&WordValidationError::TooLong(length))),
+                "{label}",
+            );
+        }
+    }
+
+    #[gtest]
+    fn invalid_mult_with_word_fails() {
+        let short_word = Word::new([
+            (1, Some(3), Sign::Positive),
+            (2, None, Sign::Negative),
+            (1, Some(2), Sign::Positive),
+        ])
+        .unwrap();
+        let long_word = Word::try_from(vec![
+            Letter::new(1, None::<u16>, Sign::Positive).unwrap();
+            u16::MAX as usize
+        ])
+        .unwrap();
+
+        let invalid_products = [
+            (
+                short_word.clone() * long_word.clone(),
+                u16::MAX as u32 + 5,
+                "short_word * long_word",
+            ),
+            (
+                long_word.clone() * short_word.clone(),
+                u16::MAX as u32 + 5,
+                "long_word * short_word",
+            ),
+            (
+                long_word.clone() * long_word.clone(),
+                2 * (u16::MAX as u32),
+                "long_word * long_word",
+            ),
+        ];
+        for (invalid_product, length, label) in invalid_products {
+            expect_that!(
+                invalid_product,
+                err(eq(&WordValidationError::TooLong(length))),
+                "{label}",
+            );
+        }
     }
 }
