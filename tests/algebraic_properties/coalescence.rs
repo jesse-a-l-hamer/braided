e0af5e2ba2109e61e3239825b6503b4c21f88922
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-10 17:23:40 -0500

    test(algebraic_properties::coalescence): implement integration tests to check band coalescence relation

---
+++ tests/algebraic_properties/coalescence.rs
@@ -0,0 +1,58 @@
+//! Integration tests which check that band coalescence works as expected.
+
+use googletest::matchers::eq;
+use googletest::{expect_that, gtest};
+
+use braided::{braid, word};
+
+#[gtest]
+fn coalescence_works_as_expected_on_words() {
+    let test_cases = [
+        (
+            word![[1; -1], [2; 1], [1; 1], [2; -1], [1; 1]].clone_unwrap(),
+            word![[1 => 3; 1], [2; -1], [1; 1]].clone_unwrap(),
+        ),
+        (
+            word![[2; 1], [1; 1], [2; -1], [2; -1], [1; 1]].clone_unwrap(),
+            word![[1 => 3; 1], [2; -1], [1; 1]].clone_unwrap(),
+        ),
+        (
+            word![[1; -1], [2; -1], [3; -1], [4; -1], [3; 1], [2; 1], [1; 1]].clone_unwrap(),
+            word![[1 => 5; -1]].clone_unwrap(),
+        ),
+        (
+            word![[4; 1], [3; 1], [1; -1], [2; -1], [3; -1], [1; 1], [4; -1]].clone_unwrap(),
+            word![[1 => 5; -1]].clone_unwrap(),
+        ),
+    ];
+
+    for (word, expected) in test_cases {
+        expect_that!(word.coalesce(), eq(&expected));
+    }
+}
+
+#[gtest]
+fn coalescence_works_as_expected_on_braids() {
+    let test_cases = [
+        (
+            braid![(); [1; -1], [2; 1], [1; 1], [2; -1], [1; 1]].clone_unwrap(),
+            braid![(); [1 => 3; 1], [2; -1], [1; 1]].clone_unwrap(),
+        ),
+        (
+            braid![(); [2; 1], [1; 1], [2; -1], [2; -1], [1; 1]].clone_unwrap(),
+            braid![(); [1 => 3; 1], [2; -1], [1; 1]].clone_unwrap(),
+        ),
+        (
+            braid![(); [1; -1], [2; -1], [3; -1], [4; -1], [3; 1], [2; 1], [1; 1]].clone_unwrap(),
+            braid![(); [1 => 5; -1]].clone_unwrap(),
+        ),
+        (
+            braid![(); [4; 1], [3; 1], [1; -1], [2; -1], [3; -1], [1; 1], [4; -1]].clone_unwrap(),
+            braid![(); [1 => 5; -1]].clone_unwrap(),
+        ),
+    ];
+
+    for (braid, expected) in test_cases {
+        expect_that!(braid.coalesce(), eq(&expected));
+    }
+}
