f04b498bda44da7fc292fc5d09dea2e7f9cfa74a
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-10 16:12:07 -0500

    test(api::generators): implement integration tests to check invariance with respect to generator type

--- tests/api/generators.rs
+++ tests/api/generators.rs
@@ -1,25 +1,107 @@
 //! Integration tests to ensure that the `Letter` interface upholds generator interoperability
 //! guarantees.
 
-use googletest::matchers::{eq, err, ok};
-use googletest::{assert_that, expect_that, gtest};
+use googletest::matchers::{eq, is_true};
+use googletest::{expect_that, gtest};
 
-use braided::{ArtinGenerator, BandGenerator, Letter, Sign, letter};
+use braided::{Letter, letter};
 
-#[gtest]
-fn can_construct_letter_from_any_generator() {}
-
-#[gtest]
-fn can_compare_any_two_letters_for_equality() {}
-
-#[gtest]
-fn can_convert_artin_letter_to_band_letter() {}
+fn equal_letter_pairs() -> [(Letter, Letter); 2] {
+    [
+        (letter![1; +].unwrap(), letter![1 => 2; +].unwrap()),
+        (letter![2; -].unwrap(), letter![2 => 3; -].unwrap()),
+    ]
+}
 
 #[gtest]
-fn can_convert_valid_band_letter_to_artin_letter() {}
+fn equality_of_letters_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left == right, is_true());
+    }
+}
 
 #[gtest]
-fn attempting_to_convert_invalid_band_letter_to_artin_fails() {}
+fn decompose_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.decompose(), eq(&right.decompose()));
+    }
+}
 
 #[gtest]
-fn can_decompose_band_letter_into_artin_letters() {}
+fn computation_of_sign_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.sign(), eq(right.sign()));
+    }
+}
+
+#[gtest]
+fn computation_of_foot_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.foot(), eq(right.foot()));
+    }
+}
+
+#[gtest]
+fn computation_of_head_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.head(), eq(right.head()));
+    }
+}
+
+#[gtest]
+fn computation_of_inverse_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.inverse(), eq(right.inverse()));
+    }
+}
+
+#[gtest]
+fn computation_of_is_artin_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.is_artin(), eq(right.is_artin()));
+    }
+}
+
+#[gtest]
+fn computation_of_height_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.height(), eq(right.height()));
+    }
+}
+
+#[gtest]
+fn computation_of_artin_length_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(left.artin_length(), eq(right.artin_length()));
+    }
+}
+
+#[gtest]
+fn computation_of_minimal_required_braid_index_does_not_depend_on_generator_type() {
+    let equal_pairs = equal_letter_pairs();
+
+    for (left, right) in equal_pairs {
+        expect_that!(
+            left.minimal_required_braid_index(),
+            eq(right.minimal_required_braid_index())
+        );
+    }
+}
