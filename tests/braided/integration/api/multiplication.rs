51bd74f86701006d4d0ed7be11e80d755ae4346c
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-22 09:52:22 -0500

    test(integration::api::multiplication): add property-based test for non-cancelling multiplication

--- tests/braided/integration/api/multiplication.rs
+++ tests/braided/integration/api/multiplication.rs
@@ -1,12 +1,34 @@
 //! Integration tests for the multiplication interface.
 
-use braided_utils::telemetry::start_tracing;
 use braided::{
     Braid, BraidIndex, BraidResult, BraidValidationError, Letter, Sign, Word, WordValidationError,
     braid, letter, word,
 };
+use braided_utils::arbitrary::valid;
+use braided_utils::telemetry::start_tracing;
 use googletest::matchers::{eq, err, ok};
 use googletest::{assert_that, expect_that, gtest};
+use proptest::prelude::*;
+
+#[test]
+fn non_cancelling_multiplication_of_valid_operands_succeeds() {
+    start_tracing();
+    let mut test_runner = prop::test_runner::TestRunner::default();
+
+    test_runner
+        .run(
+            &valid::multiplication::test_cases::non_cancelling_product(None, None),
+            |test_case| {
+                let data = test_case.data;
+                let expected = test_case.expected;
+
+                assert_that!(data.left * data.right, eq(&expected));
+
+                Ok(())
+            },
+        )
+        .unwrap();
+}
 
 // LETTERS
 #[gtest]
